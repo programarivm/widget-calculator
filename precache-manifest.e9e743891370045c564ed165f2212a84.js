1c5c21ef502947feba1fd6098be874c802cb86d1
programarivm <info@programarivm.com>
2019-11-18 20:15:45 +0000

    Updates

---
+++ precache-manifest.e9e743891370045c564ed165f2212a84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1532bda6282fdd00d5eb396b468c023c",
+    "url": "/widget-calculator/index.html"
+  },
+  {
+    "revision": "b630b22a5241e1c960be",
+    "url": "/widget-calculator/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "9d35691eae593f6825d4",
+    "url": "/widget-calculator/static/css/main.1a8d6906.chunk.css"
+  },
+  {
+    "revision": "b630b22a5241e1c960be",
+    "url": "/widget-calculator/static/js/2.3d0bf855.chunk.js"
+  },
+  {
+    "revision": "9d35691eae593f6825d4",
+    "url": "/widget-calculator/static/js/main.b5b40614.chunk.js"
+  },
+  {
+    "revision": "bcd4bbaceca5a425606e",
+    "url": "/widget-calculator/static/js/runtime-main.e27bd364.js"
+  }
+]);
